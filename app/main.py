965089d27cef5711f611df1d4b0af1b2c7a3d5bb
Shishir Lamichhane <shishirlamichhane718@gmail.com>
2024-09-08 16:41:34 +0530

    added match case positive and negative

--- app/main.py
+++ app/main.py
@@ -1,12 +1,36 @@
 import sys
 
+# import pyparsing
+# import lark - available if you need it!
+
+
 def match_pattern(input_line, pattern):
-    if len(pattern) == 1:
-        return pattern in input_line
-    elif pattern == "\\d":
+    # Handle simple character class patterns
+    if pattern == "\\d":
         return any(c.isdigit() for c in input_line)
+    elif pattern == "\\w":
+        return any(c.isalnum() for c in input_line)
+    
+    elif '[' in pattern and ']' in pattern:
+        if pattern.startswith('[^'):
+            start = 2
+        else:
+            start = 1
+        
+        end = pattern.index(']')
+        value_inside_brackets = pattern[start:end]
+        
+        if pattern.startswith('[^'):
+            return any(char not in value_inside_brackets for char in input_line)
+        else:
+            return any(char in value_inside_brackets for char in input_line)
+    elif '\\' in pattern :
+        print(pattern)
+        return 0
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
+
+
 
 
 def main():
@@ -16,11 +40,8 @@ def main():
     if sys.argv[1] != "-E":
         print("Expected first argument to be '-E'")
         exit(1)
-
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
+    
+    print("Logs from your program will appear here!")    
     
     if match_pattern(input_line, pattern):
         exit(0)
